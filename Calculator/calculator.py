3d0e5d26d41636fed8eb49943837e6b4a9fb1d8e
areeba-cyber <arebareeba409@gmail.com>
2026-06-20 14:07:47 +0500

    Calculator

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,3 +1,7 @@
+import os
+from ast import While
+
+
 def add(a,b):
     return(a + b)
 
@@ -16,13 +20,29 @@ operations_dict = {
     "*" : multiply,
     "/" : divide
 }
-
-number1 = int(input("Enter First number:"))
-for symbol in operations_dict:
+def Calculator():
+  number1 = float(input("Enter First number:"))
+  for symbol in operations_dict:
     print(symbol)
-op_symbol = input("Pick an operation from the above symbols:")
-number2 = int(input("Enter Second number:"))
 
-calculator_function = operations_dict[op_symbol]
-output =calculator_function(number1, number2)
-print(f"{number1} {op_symbol} {number2} = {output}")
+  continue_calculating = True
+  while continue_calculating:  
+   op_symbol = input("Pick an operation from the above symbols:")
+   number2 = float(input("Enter Second number:"))
+   calculator_function = operations_dict[op_symbol]
+   output =calculator_function(number1, number2)
+   print(f"{number1} {op_symbol} {number2} = {output}")
+
+   Continue=input(f"Type 'y' to continue calculating with {output}, or type 'n' to start a new calculation or 'x' to exit.").lower()
+   if Continue == "y":
+     number1 = output
+   elif Continue == "n":
+        continue_calculating = False
+        os.system('cls')
+        Calculator()
+        # print("Starting a new calculation...")
+   else:
+        continue_calculating = False
+        print("Bye...")
+
+Calculator()
