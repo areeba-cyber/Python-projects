2453a59660a4f0bbb03dba65bc7ff9eb93128a0c
areeba-cyber <arebareeba409@gmail.com>
2026-07-06 15:29:02 +0500

    weather

--- Weather app/weather.py
+++ Weather app/weather.py
@@ -16,4 +16,4 @@ if data["cod"] == 200:
     print(f"\n Weather in {city_name}")
     print(f"Temperature: {temperature}°C")
     print(f"Humidity : {humidity}%")
-    
+    print(f"Weather: {weather}")
