93e35d808d00c5dab14b4285fe4130afe55d1df6
areeba-cyber <arebareeba409@gmail.com>
2026-07-06 15:17:46 +0500

    data into json

--- Weather app/weather.py
+++ Weather app/weather.py
@@ -5,3 +5,6 @@ API_key = '9b3f643fb121093e79165f75b00edd9e'
 
 url = "https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}&units={metric}"
 
+response = requests.get(url)
+data = response.json()
+
