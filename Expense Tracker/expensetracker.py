586b8ac51162601aba19cb74b4cdfdc0ebd8051c
areeba-cyber <arebareeba409@gmail.com>
2026-07-07 12:18:21 +0500

    Expense tracker

--- Expense Tracker/expensetracker.py
+++ Expense Tracker/expensetracker.py
@@ -2,5 +2,9 @@ expenses = []
 
 while True:
     print("\n===== Expense Tracker =====")
+    print("1. Add Expense")
+    print("2. View Expense")
+    print("3. Total expense")
+    print("4. End")
 
 
