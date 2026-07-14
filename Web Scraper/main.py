4a719279d0de1bc67702ca1cc9adf6b0cd9d5d82
areeba-cyber <arebareeba409@gmail.com>
2026-07-14 16:05:08 +0500

    course price

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -7,3 +7,4 @@ with open('home.html', 'r') as html_file:
    course_cards = soup.find_all('div', class_= 'card')
    for course in course_cards:
       course_name = course.h5.text
+      course_price = course.a.text.split()[-1]
