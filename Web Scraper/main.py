4ff8f8a1f9e694858c1cefbd924d419f9107c2b7
areeba-cyber <arebareeba409@gmail.com>
2026-07-14 16:04:13 +0500

    course tags

--- Web Scraper/main.py
+++ Web Scraper/main.py
@@ -6,4 +6,4 @@ with open('home.html', 'r') as html_file:
    soup = BeautifulSoup(content, 'lxml')
    course_cards = soup.find_all('div', class_= 'card')
    for course in course_cards:
-      
+      course_name = course.h5.text
