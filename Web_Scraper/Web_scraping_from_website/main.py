43db627ce80e17fbd821a279654d74649812f62e
areeba-cyber <arebareeba409@gmail.com>
2026-07-18 16:54:49 +0500

    website url

--- Web_Scraper/Web_scraping_from_website/main.py
+++ Web_Scraper/Web_scraping_from_website/main.py
@@ -1,4 +1,4 @@
 from bs4 import BeautifulSoup
 import requests
 
-html_text = requests.get()
+html_text = requests.get('https://www.timesjob.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=')
